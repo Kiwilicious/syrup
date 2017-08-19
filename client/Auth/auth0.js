fe231dde9cb16514879bb0fd3ff7602e66f45a4f
Kiwilicious <jhwang137@gmail.com>
2017-08-19 09:14:54 -0700

    Edited auth

--- client/Auth/auth0.js
+++ client/Auth/auth0.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-    domain: 'stephaniefu.auth0.com',
-    clientID: 'y54Mz7sw5E2UCqDnbaxzyVsy7izLtAK5',
+    domain: 'taeminpak.auth0.com',
+    clientID: 'Gn9QRrr1wAJou5J2E41RTpzQV4rZk4lO',
     redirectUri: 'https://syrup-.herokuapp.com/upload',
 }
